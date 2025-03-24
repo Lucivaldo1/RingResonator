15b0ce2be475dbc7e591e2047a34ae5131dbb2f8
Lucivaldo <135773154+Lucivaldo1@users.noreply.github.com>
2025-03-24 19:56:33 -0300

    documentando o arquivo tools e adicionando arquivos de apresentacao

--- tools.py
+++ tools.py
@@ -1,13 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.constants import c
+
 '''
+@author: Lucivaldo Aguiar 
 References:
 https://optics.ansys.com/hc/en-us/articles/360042819293-Coupled-ring-resonator-filters
 Integrated ring resonator, Dominik G. Rabus
 Synthesis of a parallel-coupled ring-resonator filter, Andrea Melloni
 '''
 def FSR2graph (FSR):
+    #this graphic is valid only and if only m1 - m2 = 1
     nm = 1e-9
     um = 1e-6
     FSR1 = np.linspace(0.1*nm,15*nm, 100)
@@ -22,7 +25,18 @@ def FSR2graph (FSR):
     plt.show()
     
 def ringData(x, N,ng, FSR):
-    #x is the relation FSR/B
+    """
+       Computes key parameters for a ring resonator based on input parameters.
+
+       Inputs:
+           x   : Relation FSR/B (Free Spectral Range to Bandwidth ratio)
+           N   : Number of rings
+           ng  : Group index of the waveguide
+           FSR : Free Spectral Range
+
+       Output:
+           A dictionary containing calculated values for FSR, K, Q, g, B, and L.
+       """
     dictionary ={
         'FSR' : [],
         'K' : [],
@@ -31,40 +45,34 @@ def ringData(x, N,ng, FSR):
         'B' : [],
         'L' : []
         }
-    #determining FSRs
+    #determining FSRs and ring total length
     if N > 1:
         m1 = 3
         m2 = 2
         FSR1 = FSR/m1
         FSR2 = (m1/m2)*FSR1
-        dictionary['FSR'].append(FSR1)
-        dictionary['FSR'].append(FSR2)
+        dictionary['FSR'].extend([FSR1,FSR2])
         #ring total length
         l1 = (1550e-9**2)/(ng*FSR1)
         l2 = (1550e-9**2)/(ng*FSR2)
-        dictionary['L'].append(l1)
-        dictionary['L'].append(l2)
+        dictionary['L'].extend([l1,l2])
     else:
         dictionary['FSR'].append(FSR)
         l = (1550e-9**2)/(ng*FSR)
         dictionary['L'].append(l)
         
     for n in range(1,3):
-        #segundo o artigo Synthesis of a parallel-coupled
-        #ring-resonator filter Andrea Melloni FSR/B = 10, 30, 120...
-        B = FSR/x
+        #Computing parameters based on Melloni's method
         g = np.sqrt(2) * np.sin((2*n-1)/(2*N) * np.pi)
+        B = FSR / x
         Q = FSR/(B*g)
         K = ((np.pi**2)/(2*Q**2))*(np.sqrt(1 + (4*Q**2)/(np.pi**2)-1))
+
         dictionary['g'].append(g)
         dictionary['Q'].append(FSR/(B*g))    
         dictionary['K'].append(K)
         dictionary['B'].append(B)
-    #i'm calling it k3 due to index convention, but it's actually k2
-    #a.k.a the coupling coefficient of the ring that goes in the middle
+    #Computing coupling coefficient for the middle ring
     dictionary['K'].append(np.sqrt(0.25)*(dictionary['K'][0])**2)
     return dictionary
-
-#FSR2graph(20e-9)
-#print(ringData(30,1,4.355, 1e-9))
 
